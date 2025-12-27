6472a8f10a76d78d8b8797da0c8d3caa8e788747
abusayadebu <abusayad743@gmail.com>
2025-12-27 19:57:32 +0600

    all cards functionality added

--- js/donation.js
+++ js/donation.js
@@ -2,19 +2,46 @@ const donateActionButton = document.querySelectorAll(".doante-action-btn")
 
 for(const button of donateActionButton){
     // noakhaliBtn clicked
-    const NoakhaliBtn = button.addEventListener("click", function(){
+    const noakhaliBtn = button.addEventListener("click", function(){
         const noakhaliInputNumber = getInputFieldValueById("noakhali-input");
+        const feniInputNumber = getInputFieldValueById("feni-input")
+        const quotaInputNumber = getInputFieldValueById("quota-input");
+
+        // take main balance
         const mainBalance = getTextValuebyId("main-balance"); 
-        const mainUpdateBalance = mainBalance - noakhaliInputNumber;
 
-        // show the main balance
-        document.getElementById("main-balance").innerText = mainUpdateBalance;
-        const noaKhaliDonation = getTextValuebyId("noakhali-donation")
+        // take every donations text number
+        const noaKhaliDonation = getTextValuebyId("noakhali-donation");
+        const feniDonation = getTextValuebyId("feni-donation");
+        const quotaDonation = getTextValuebyId("quota-donation");
 
-        const noaKhaliDonationtotal = noaKhaliDonation + noakhaliInputNumber;
-        
-        // show the noakhali total balance-
-        document.getElementById("noakhali-donation").innerText = noaKhaliDonationtotal;
+        // noakhali btn
+        if(button.id === "noakhali-btn"){
+            const updateBalance = mainBalance - noakhaliInputNumber;
+            const updateNoakhali = noaKhaliDonation + noakhaliInputNumber;
+            // show main and noakhali balance
+            document.getElementById("main-balance").innerText = updateBalance;
+            document.getElementById("noakhali-donation").innerText = noakhaliInputNumber;
+        }
+        // feni btn
+         else if (button.id === "feni-btn") {
+
+            const updatedMain = mainBalance - feniInputNumber;
+            const updatedFeni = feniDonation + feniInputNumber;
+
+            document.getElementById("main-balance").innerText = updatedMain;
+            document.getElementById("feni-donation").innerText = updatedFeni;
+        }
+
+        // Quota button
+        else if (button.id === "quota-btn") {
+
+            const updatedMain = mainBalance - quotaInputNumber;
+            const updatedQuota = quotaDonation + quotaInputNumber;
+
+            document.getElementById("main-balance").innerText = updatedMain;
+            document.getElementById("quota-donation").innerText = updatedQuota;
+        }
         
     })
 
